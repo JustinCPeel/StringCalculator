52d1b348da0e34cbea57dd498f09fa8a3b5b2c72
Justin Peel <justinpeel25@gmail.com>
2020-04-20 14:25:53 +0200

    Initial Commit

---
+++ String_Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("String_Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("String_Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58fb0f27-dc8d-4d95-93e0-2243ed8e3e4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ String_Calculator/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit;
+using NUnit.Framework;
+
+namespace String_Calculator
+{
+	[TestFixture]
+	public class UnitTest1
+	{
+		[Test]
+		public void GivenNumbers_WithBlankSpace_ShouldReturn0()
+		{
+			//arrange
+			var numbers = "";
+
+			//act
+			int expected = 0;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithOneValue_ShouldReturn1()
+		{
+			//arrange
+			var numbers = "1";
+
+			//act
+			int expected = 1;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithTwoValues_ShouldReturn3()
+		{
+			//arrange
+			var numbers = "1,2";
+
+			//act
+			int expected = 3;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithThreeValues_ShouldReturn6()
+		{
+			//arrange
+			var numbers = "1,2,3";
+
+			//act
+			int expected = 6;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithNewLinesBetweenValues_ShouldReturn6()
+		{
+			//arrange
+			var numbers = "1\n2,3";
+
+			//act
+			int expected = 6;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithNewDelimiterValues_ShouldReturn6()
+		{
+			//arrange
+			var numbers = "|1\n2|3";
+
+			//act
+			int expected = 6;
+			int actual = Add(numbers);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenNumbers_WithNegativeValues_ShouldReturnError()
+		{
+			//arrange
+			var numbers = "-1\n-2,-3";
+
+			//assert
+			Assert.Throws<Exception>(() => Add(numbers));
+		}
+
+
+		private int Add(string numbers)
+		{
+			if (string.IsNullOrEmpty(numbers))
+				return 0;
+
+			var delimiters = new List<char>() { ',', '\n' };
+
+
+			if (!char.IsDigit(numbers[0]) && numbers[0] != '-')
+				delimiters.Add(numbers[0]);
+
+			var sumOfNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList().ConvertAll(int.Parse);
+
+			if (sumOfNumbers.Any(num => num < 0))
+				throw new Exception($"Negatives Not Allowed : {string.Join(",", sumOfNumbers.Where(num => num < 0))}");
+
+			return sumOfNumbers.Sum();
+		}
+	}
+}
